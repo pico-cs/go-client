b468133bc8233f335b06224588a5886e169268bf
sm <stefan.miller@sap.com>
2022-11-17 17:54:17 +0100

    go-client v0.1.10

--- client/conn.go
+++
@@ -1,8 +0,0 @@
-package client
-
-// Conn is a stream orianted connection to the pico board.
-type Conn interface {
-	Read(p []byte) (n int, err error)
-	Write(p []byte) (n int, err error)
-	Close() error
-}
